2f8f561df60ba3b3b73f7e0974daf635b07f82c8
Hamza Haji <tiyya1017@gmail.com>
2022-03-11 13:55:17 -0800

    Write a program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
+ *main - Writing a program that prints numbers from 0 to 99.
+ *Return: The value will return 0.
  */
+
 int main(void)
 {
-int i, j;
-int a, b, c, d;
+int i, j, k, l, f, s;
 
-for (i = 0; i < 100; i++)
+for (f = 0; f < 100; f++)
 {
-a = i / 10; /* doubles fnum */
-b = i % 10; /* singles fnum */
-for (j = 0; j < 100; j++)
+for (s = 0; s < 100; s++)
 {
-c = j / 10; /* doubles snum */
-d = j % 10; /* singles snum */
-
-if (a < c || (a == c && b < d))
+if (f < s)
 {
-putchar(a + '0');
-putchar(b + '0');
-putchar(32);
-putchar(c + '0');
-putchar(d + '0');
-
-if (!(a == 9 && b == 8)
+l = s % 10;
+k = s / 10;
+j = f % 10;
+i = f / 10;
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+if (f == 98 && s == 99)
 {
-putchar(44);
-putchar(32);
+break;
+}
+putchar(',');
+putchar(' ');
 }
 }
-}
-}
-putchar(10);
 
+}
+putchar('\n');
 return (0);
 }
